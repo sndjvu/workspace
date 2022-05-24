a76ff415880d24082dfd597d4f1d6f31bbb505c1
Cole Miller <m@cole-miller.net>
2022-05-24 17:36:46 -0400

    sndjvu_format: basic types and constants

---
+++ crates/sndjvu_format/src/lib.rs
@@ -0,0 +1,69 @@
+#![no_std]
+
+extern crate alloc;
+#[cfg(feature = "std")]
+extern crate std;
+
+pub struct TxtVersion(pub u8);
+
+impl TxtVersion {
+    pub const CURRENT: Self = Self(1);
+}
+
+pub struct Iw44Version {
+    _major: u8,
+    _minor: u8,
+}
+
+impl Iw44Version {
+    pub const CURRENT: Self = Self { _major: 1, _minor: 2 };
+}
+
+pub struct FgbzVersion(u8);
+
+impl FgbzVersion {
+    pub const CURRENT: Self = Self(0);
+}
+
+pub struct PageVersion {
+    pub major: u8,
+    pub minor: u8,
+}
+
+impl PageVersion {
+    pub const CURRENT: Self = Self { major: 0, minor: 26 };
+}
+
+pub enum PageRotation {
+    Up = 1,
+    Ccw = 6,
+    Down = 2,
+    Cw = 5,
+}
+
+pub struct MultiPageVersion(u8);
+
+impl MultiPageVersion {
+    pub const CURRENT: Self = Self(1);
+}
+
+pub enum ComponentKind {
+    Djvi,
+    Djvu,
+    Thum,
+}
+
+pub enum ZoneKind {
+    Page = 1,
+    Column,
+    Region,
+    Paragraph,
+    Line,
+    Word,
+    Character,
+}
+
+pub enum Iw44ColorSpace {
+    YCbCr,
+    Gray,
+}
