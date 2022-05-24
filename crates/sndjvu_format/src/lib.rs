ee54a74ef9bd73e8d185c00226fa741a70cdb596
Cole Miller <m@cole-miller.net>
2022-05-24 19:56:52 -0400

    Deny some lints

--- crates/sndjvu_format/src/lib.rs
+++ crates/sndjvu_format/src/lib.rs
@@ -1,4 +1,11 @@
 #![no_std]
+#![deny(
+    elided_lifetimes_in_paths,
+    unsafe_op_in_unsafe_fn,
+    unused_must_use,
+
+    clippy::pattern_type_mismatch,
+)]
 
 extern crate alloc;
 #[cfg(feature = "std")]
