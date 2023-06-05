fdb5b139ffa0081f4d7a33387633faecccaf5b2a
Cole Miller <m@cole-miller.net>
2023-06-04 21:59:11 -0400

    Publish highlighted sndjvu.h

--- x.sh
+++ x.sh
@@ -2,10 +2,16 @@
 
 set -o errexit -o nounset
 
+ARCH="${ARCH:-linux-amd64}"
+
 git config user.name "Cole Miller"
 git config user.email "m@cole-miller.net"
 
 export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
+
+deps() {
+	curl -L "https://github.com/alecthomas/chroma/releases/download/v2.7.0/chroma-2.7.0-$ARCH.tar.gz" | tar xzf - chroma
+}
 
 check() {
 	cargo test --workspace --all-features --no-run --profile ci
@@ -23,6 +29,9 @@ www() {
 	mkdir www/rustdoc
 	cp -r target/x86_64-unknown-linux-gnu/doc/* www/rustdoc
 
+	cp crates/sndjvu/sndjvu.h www/sndjvu.h
+	./chroma -l c -s friendly -f html --html-inline-styles <crates/sndjvu/sndjvu.h >www/sndjvu.h.html
+
 	find . -maxdepth 1 -mindepth 1 '!' '(' -name .git -or -name www ')' -exec rm -r '{}' '+'
 	find www -maxdepth 1 -mindepth 1 -exec mv -t . '{}' '+'
 	rmdir www
@@ -37,6 +46,7 @@ www() {
 }
 
 ci() {
+	deps
 	check
 	www
 }
